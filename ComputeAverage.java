5a155a8d92c6be5555da66a44c718a52effded61
Chris Cottingham <chris@ccotti.com>
2019-07-07 13:16:54 -0500

    added intitial files

---
+++ ComputeAverage.java
@@ -0,0 +1,27 @@
+//package Chapter2;
+
+/**
+ * @author CCottingham
+ */
+
+//package java101;
+//import java.util.*;
+//
+//public class ComputeAverage {
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        
+//        //Prompt User for Input
+//        System.out.print("Enter three numbers: ");
+//        double number1 = input.nextDouble();
+//        double number2 = input.nextDouble();
+//        double number3 = input.nextDouble();
+//        
+//        //Compute Average
+//        double average = (number1 + number2 + number3) / 3;
+//        
+//        //Display Results
+//        System.out.println("The average of " + number1 + " and " + number2 + " and " + number3 + " is " + average);
+//    }
+//    
+//}
