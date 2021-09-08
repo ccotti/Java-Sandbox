643461289cf107b6675a3828df644aa7e015ce86
Chris Cottingham <chris@ccotti.com>
2021-09-08 13:07:50 -0500

    Java Sandbox Clean Up and Initialization

---
+++ sandbox02/a2p2BarChart.java
@@ -0,0 +1,34 @@
+
+// import java.util.Scanner;
+
+// public class a2p2BarChart {
+    
+// 	public static void main(String [] args) {
+		
+// 		System.out.println("Chris Cottingham – Assignment 3\n");
+// 		//Substitute your name for student name and 
+// 		//the lab number for #
+		
+// 		Scanner input = new Scanner(System.in);
+		
+// 		System.out.print("Enter 5 numbers between 1 and 30: ");
+		
+// 		String a = "* ";
+		
+// 			for (int i = 1; i < 6; i++) {
+				
+// 				int num = input.nextInt();
+			
+// 			for (int j = 1; j <= num; j++)	{
+				
+// 				System.out.printf("%s", a);
+// 			}
+			
+// 			System.out.println();
+// 		}		
+	
+// 		input.close();
+		
+// 	} // end of main		
+		
+// } // end of class
---
+++ sandbox02/a4p2CircleArea.java
@@ -0,0 +1,39 @@
+// import java.util.Scanner;
+
+// public class a4p2CircleArea {
+//     public static void main(String [] args) {
+		
+// 		System.out.println("Chris Cottingham – Assignment 4\n");
+// 		//Substitute your name for student name and 
+// 		//the lab number for #
+		
+// 		// Variables to be used for input and calculations
+// 		double radius = 0.0;
+		
+// 		Scanner input = new Scanner(System.in);
+		
+// 		// Prompt User for Radius
+// 		System.out.print("Enter the radius of the circle: "); 
+// 		radius = input.nextDouble();
+		
+// 		double result = circleArea(radius); // call method and send value
+		
+// 		//Print result
+// 		System.out.printf("The area of the circle is: %.2f\n ", result);
+		
+// 		input.close();
+		
+// 	} // end of main
+
+// 	// Method used to determine the area of a circle given the radius
+// 	public static double circleArea(double r) {
+		
+// 		final double pi = 3.14159; // pi as a constant
+		
+// 		double area = pi * Math.pow(r , 2); // calculate area
+		
+// 		return area; // return value
+		
+// 	} // end of method
+	
+// } // end of class
