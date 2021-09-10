f139f4b066d51722854a1f97ccb480529ae785b7
Chris Cottingham <chris@ccotti.com>
2021-09-10 10:47:15 -0500

    change to section2 file

---
+++ udemyJava/Section 2/course/src/section2.java
@@ -0,0 +1,29 @@
+ /* create code for 
+        * Using Variables, Strings, While & For loops, If stmnt, User Input, Do While, and Switch stmnt
+       */
+
+import java.util.Scanner;
+
+public class section2 {
+    
+    public static void main(String[] args) {
+      
+    
+        String text = "Hello";
+        Integer num1 = 0;
+        Integer num2 = 0;
+
+        Scanner unum1 = new Scanner();
+        num1 = unum1.nextInt();
+        Scanner unum2 = new Scanner();
+        num2 = unum2.nextInt();
+
+        
+
+
+    
+    
+    
+    
+    }
+}
