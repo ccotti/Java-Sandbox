ca14aa18bfcb15ad15e4ce8fc25f98235fbfac10
Chris Cottingham <chris@ccotti.com>
2021-09-19 22:30:40 -0500

    Array Practice

---
+++ udemy/ArrayListFun.java
@@ -0,0 +1,19 @@
+// import java.util.*
+
+// ;public class ArrayListFun {
+//     public static void main(String[] args){
+
+//         ArrayList<String> namesList = new ArrayList<>();
+
+//         namesList.add("John");
+//         namesList.add("Kyle");
+//         namesList.add("Matthew");
+//         namesList.add("Amanda");
+//         namesList.add("Wendy");
+
+//         for(int i = 0; i < namesList.size(); i ++) {
+//             System.out.println(namesList.get(i));
+//         }
+
+//     } // end main
+// }
---
+++ udemy/num9_Arrays.java
@@ -0,0 +1,44 @@
+// public class num9_Arrays {
+//     public static void main(String[] args) {
+
+//         int[] values = new int[3];
+//         int[] myArray = new int[10];
+
+//         // System.out.println(values[0]);
+
+//         // values[0] = 10;
+//         // values[1] = 20;
+//         // values[2] = 30;
+
+//         // System.out.println(values[0]);
+//         // System.out.println(values[1]);
+//         // System.out.println(values[2]);
+
+//         for(int i = 0; i<values.length; i++) {
+//             System.out.println(i);
+//         }
+
+//         System.out.println();
+
+//         int[]numbers = {5,6,7};
+
+//         // for(int i=0; i<numbers.length; i++){
+//         //     System.out.println(numbers[i]);
+//         // }
+
+//         for(int num : numbers) {
+//             System.out.println(num);
+//         }
+
+//         System.out.println();
+
+//         for(int i = 0; i < myArray.length; i++) {
+//             myArray[i] = i +1;
+//         }
+
+//         for(int num : myArray) {
+//             System.out.println(num);
+//         }
+
+//     } // end main
+// } // end class
