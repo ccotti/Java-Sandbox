c18794df955dd93a3cc8ea194a378d6b97700dd7
Chris Cottingham <chris@ccotti.com>
2021-09-21 23:11:59 -0500

    Create Proj4_3_Parallel.java

---
+++ udemy/Proj4_3_Parallel.java
@@ -0,0 +1,35 @@
+// import java.util.ArrayList;
+// import java.util.Scanner;
+
+// public class Proj4_3_Parallel {
+//     public static void main(String[] args) {
+
+//         Scanner keyboard = new Scanner(System.in);
+//         ArrayList<String> names = new ArrayList<>();
+//         ArrayList<Integer> ages = new ArrayList<>();
+        
+//         String name;
+//         int age;
+
+//        for(int i = 0; i < 5; i++) {
+//         System.out.print("Enter the name:\t");
+//         name = keyboard.nextLine();
+//         System.out.print("Enter the age:\t");
+//         age = keyboard.nextInt();
+
+//         keyboard.nextLine();
+
+//         names.add(name);
+//         ages.add(age);
+
+//         System.out.println();
+
+//        } // for #1
+
+//        for(int i = 0; i < names.size(); i++) {
+//             System.out.println(names.get(i) + " is " + ages.get(i) + " years old");
+
+//        } // end for #2
+//         keyboard.close();
+//     } // end main
+// } // end class
