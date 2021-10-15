b411a63307e9823f1ee4753f58a1cb8a6ccae9ee
Chris Cottingham <chris@ccotti.com>
2021-10-15 14:15:42 -0500

    String Projects

--- udemy/Proj5_2_StringsP2.java
+++ udemy/Proj5_2_StringsP2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Proj5_2_StringsP2 {
     public static void main(String[] args) {
         String myName = "Chris Cottingham";
@@ -5,14 +7,35 @@ public class Proj5_2_StringsP2 {
         String upper = myName.toUpperCase();
         String lower = myName.toLowerCase();
 
-        int whereIsN = myName.indexOf("n");
+        int whereIsSP = myName.indexOf(" ");
 
-        String LastName = myName.substring(6);
+        String LastName = myName.substring(whereIsSP);
 
         System.out.println("upper is " + upper);
         System.out.println("lower is " + lower);
-        System.out.println("N is at index " + whereIsN);
+        System.out.println("The space is at index " + whereIsSP);
         System.out.println("Last name is " + LastName);
 
+        String fullName;
+        String firstName;
+        String lastName;
+
+        Scanner keyboard = new Scanner(System.in);
+       
+        System.out.println("What is your full name?");
+        fullName = keyboard.nextLine();
+
+        int indexOfSpace = fullName.indexOf(" ");
+
+        firstName = fullName.substring(0, indexOfSpace);
+        lastName = fullName.substring(indexOfSpace + 1);
+
+        firstName = firstName.toUpperCase();
+        lastName = lastName.toLowerCase();
+
+        System.out.println("First name is " + firstName);
+        System.out.println("Last name is " + lastName);
+
+        keyboard.close();
     } // end main
 } // end class
---
+++ udemy/StringBuilderFun.java
@@ -0,0 +1,18 @@
+public class StringBuilderFun {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Chris Cottingham");
+
+        sb.append(" is awesome");
+        System.out.println(sb);
+
+        sb.insert(6, "Edward ");
+        System.out.println(sb);
+
+        sb.replace(27, 37, "amazing");
+        System.out.println(sb);
+
+        sb.delete(5, 12);
+        System.out.println(sb);
+
+    }
+}
