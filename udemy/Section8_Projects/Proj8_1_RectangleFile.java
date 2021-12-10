242176d4fd3bcb11a388411d3bcacdf4c0d6fef7
Chris Cottingham <chris@ccotti.com>
2021-12-10 16:24:25 -0600

    Project 8.2

---
+++ udemy/Section8_Projects/Proj8_1_RectangleFile.java
@@ -0,0 +1,58 @@
+package Section8_Projects;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import Section7_Projects.Rectangle;
+
+public class Proj8_1_RectangleFile {
+    public static void main(String[] args) {
+        ArrayList<Rectangle> rectangleList;
+        rectangleList = new ArrayList<>();
+        
+        fillArrayList(rectangleList);
+        printRectangles(rectangleList);
+
+    }// end main
+
+    public static void fillArrayList(ArrayList<Rectrangle> rectangleAL) {
+        Scanner infile;
+        
+        try {
+            
+            infile = new Scanner(new File("rectangle_data.txt"));
+            Rectangle temp;
+            double length;
+            double width;
+
+            while(infile.hasNext()){
+                length = infile.nextDouble();
+                width = infile.nextDouble();
+                temp = new Rectangle(length, width);
+                rectangleAL.add(temp);
+            }//end while
+
+            infile.close();
+
+        } catch (Exception e) {
+
+            System.out.println("Error accessing file!");
+
+        }//end try-catch
+    }//end fillArrayList
+
+    public static void printRectangles(ArrayList<Rectangle> rectangleAL) {
+        for(Rectangle r : rectangleAL) {
+            
+            System.out.println("Length: " + r.getLength());  
+            System.out.println("Width: " + r.getWidth());
+            System.out.println("Area: " + r.area());
+            System.out.println("Perimeter: " + r.perimeter());
+            System.out.println();
+
+        }// end for
+    }//end printRectangles
+
+
+}//end class
