336ca75bfa6c7d50698c11ae3819f54318ee84a3
Chris Cottingham <chris@ccotti.com>
2023-08-16 11:28:54 -0500

    update ConstructorPartOne

--- JavaMasterClass/ClassesChallenge/src/Account.java
+++ JavaMasterClass/ClassesChallenge/src/Account.java
@@ -6,6 +6,19 @@ public class Account {
     private String customerEmail;
     private String customerPhone;
 
+    public Account() {
+        System.out.println("Empty constructor called");
+    }
+
+    public Account(String number, double balance, String customerName, String customerEmail, String customerPhone) {
+        System.out.println("Account constructor with parameters called");
+        this.number = number;
+        this.balance = balance;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.customerPhone = customerPhone;
+    }
+
     public void depositFunds(double depositAmount) {
 
         balance += depositAmount;
